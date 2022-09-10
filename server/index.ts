46e554c9fbb1edeb728e47611d267aed1fcf156a
Lucas <lucasmruiz05@gmail.com>
2022-09-10 17:33:15 -0300

    Add expres urlencoded limit

--- server/index.ts
+++ server/index.ts
@@ -34,6 +34,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 app.get('/env', (req, res) => {
 	res.json({
